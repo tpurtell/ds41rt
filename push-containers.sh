a3aa6b141dd8254ae9b6e8ef29a0d84b1d8bdf42
T.J. Purtell <tpurtell@gmail.com>
2026-09-16 12:53:52 +0800

    Set development container tags for the v4 upstream integration release

--- push-containers.sh
+++ push-containers.sh
@@ -13,7 +13,7 @@ The supplied release tag and latest are published for both images. The
 coordinator image is local; the Spark image is published from SPARK_0_HOST.
 
 Example:
-  ./push-containers.sh v1
+  ./push-containers.sh v4
 EOF
 }
 
